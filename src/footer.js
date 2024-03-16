826e8630e94c351fd46138d64d0b5c4917a67114
BalaAtchaya2004 <atchayaramesh574@gmail.com>
2024-03-16 13:05:06 +0530

    Initial commit

---
+++ src/footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <h4>
+          
+          <a
+            style={{
+              textDecoration: "underline",
+              color: "white",
+            }}
+            href="https://BalaAtchaya2004.github.io"
+            target="_blank"
+            rel="noreferrer"
+          >
+            By Bala Atchaya R
+          </a>
+        </h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
